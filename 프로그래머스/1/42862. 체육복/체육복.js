858e09b878c39be85a4c19f2e73fee4999016ce7
visitor17564 <lanlanloo17564@gmail.com>
2023-11-09 19:39:49 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,57 +1,25 @@
 function solution(n, lost, reserve) {
-    // 여벌 옷 생성
-    lost.sort();
-    reserve.sort();
     let reserve2 = [];
     for (let value of reserve) {
         reserve2.push(value);
     }
-    console.log(reserve2);
-    console.log(lost);
-    // reserve2에서 lost 중복차감
-    for (a = lost.length - 1; a >= 0; a--) {
-        if (reserve2.indexOf(lost[a]) !== -1) {
-            reserve2.splice(reserve2.indexOf(lost[a]), 1);
-            lost.splice(a, 1);
+    for (a = 0; a <= lost.length - 1; a++) {
+        if(reserve2.indexOf(lost[a])!==-1) {
+            reserve2.splice(reserve2.indexOf(lost[a]),1);
+            lost.splice(a,1)
         }
     }
-    console.log(reserve2);
-    console.log(lost);
-    // +-1 중 하나만 있는 애들부터 처리해주기
-    // reserve2에서 lost+1값 먼저차감
-    for (a = lost.length - 1; a >= 0; a--) {
-        const findPlusSizeIndex = reserve2.indexOf(lost[a] + 1);
-        const findMinusSizeIndex = reserve2.indexOf(lost[a] - 1);
-        if (findPlusSizeIndex !== -1 && findMinusSizeIndex !== -1) {
-            continue;
-        } else if(findPlusSizeIndex !== -1){
-            reserve2.splice(findPlusSizeIndex, 1);
-            lost.splice(a, 1);
-        } else if(findMinusSizeIndex !== -1){
-            reserve2.splice(findMinusSizeIndex, 1);
-            lost.splice(a, 1);
+    for (a = 0; a <= lost.length - 1; a++) {
+        if(reserve2.indexOf(lost[a]-1)!==-1) {
+            reserve2.splice(reserve.indexOf(lost[a])-1,1);
+            lost.splice(a,1)
         }
     }
-    // reserve2에서 lost+1값 먼저차감
-    for (a = lost.length - 1; a >= 0; a--) {
-        const findPlusSizeIndex = reserve2.indexOf(lost[a] + 1);
-        if (findPlusSizeIndex !== -1) {
-            reserve2.splice(findPlusSizeIndex, 1);
-            lost.splice(a, 1);
+    for (a = 0; a <= lost.length - 1; a++) {
+        if(reserve2.indexOf(lost[a]+1)!==-1) {
+            reserve2.splice(reserve.indexOf(lost[a])+1,1);
+            lost.splice(a,1)
         }
     }
-    console.log(reserve2);
-    console.log(lost);
-
-    // reserve2에서 lost-1값 후차감
-    for (a = lost.length - 1; a >= 0; a--) {
-        const findMinusSizeIndex = reserve2.indexOf(lost[a] - 1);
-        if (findMinusSizeIndex !== -1) {
-            reserve2.splice(findMinusSizeIndex, 1);
-            lost.splice(a, 1);
-        }
-    }
-    console.log(reserve2);
-    console.log("🚀  lost:", lost);
-    return n - lost.length;
+    return n-lost.length
 }
